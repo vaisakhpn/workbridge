63d1b6462f7fc79b7aa34ffc623497297743c86a
Vaisakh <vaisakhpn78@gmail.com>
2026-07-28 21:13:07 +0530

    updated backend server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,7 +29,12 @@ connectDB();
 
 app.disable("x-powered-by");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
