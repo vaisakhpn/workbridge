c6c1de1d9be0dea184c69e6ae51f5794899a53eb
Vaisakh <vaisakhpn78@gmail.com>
2026-07-28 20:00:21 +0530

    changes with pretier

---
+++ client/src/config/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+
+  WORKER_DASHBOARD: "/worker/dashboard",
+
+  EVENT_TEAM_DASHBOARD: "/event-team/dashboard",
+
+  ADMIN_LOGIN: "/admin/login",
+
+  ADMIN_DASHBOARD: "/admin/dashboard",
+} as const;
